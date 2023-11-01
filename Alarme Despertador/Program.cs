6bacaaad48831ad43fcd28174ebba734257aa823
Pedro Lucas <plserramoura@gmail.com>
2023-11-01 10:00:20 -0300

    atualizacao

--- Alarme Despertador/Program.cs
+++ Alarme Despertador/Program.cs
@@ -9,15 +9,21 @@ class Program {
             if(linha0 == "0 0 0 0"){
                 break;
             }
-            string [] linha = linha0.Split(" ");
+            string [] linha = linha0.Split(' ');
             int h1 = int.Parse(linha[0]);
             int m1 = int.Parse(linha[1]);
             int h2 = int.Parse(linha[2]);
             int m2 = int.Parse(linha[3]);
-            if (h1 > h2 || (h1 == h2 && m1 > m2))
+            if (h1 > h2 || (h1 >= h2 && m1 >= m2))
                 h1 = h1 - 24;
 
             int resp = (h2*60+m2)-(h1*60+m1);
+            /*int hora0 = (h1*60+m1);
+            int hora1 = (h2*60+m2);
+            if (hora0 > hora1)
+                hora1 += 1440;
+
+            int resp = hora1-hora0;*/
 
             Console.WriteLine(resp);
         }while(linha0 != "0 0 0 0");
--- Distância Entre Dois Pontos/Program.cs
+++ Distância Entre Dois Pontos/Program.cs
@@ -4,8 +4,12 @@ class Program {
 
     public static void Main(string[] args) { 
         double x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-        string [] ponto1 = Console.ReadLine().Split(" ");
-        string [] ponto2 = Console.ReadLine().Split(" ");
+        string linha0 = Console.ReadLine();
+        string linha1 = Console.ReadLine();
+        string [] ponto1 = linha0.Split(' ');
+        string [] ponto2 = linha1.Split(' ');
+        //string [] ponto1 = Console.ReadLine().Split(" "); plataforma nao aceita essa formatação
+        //string [] ponto2 = Console.ReadLine().Split(" ");
         x1 = double.Parse(ponto1[0]);
         y1 = double.Parse(ponto1[1]);
         x2 = double.Parse(ponto2[0]);
@@ -13,5 +17,4 @@ class Program {
         double resp = Math.Sqrt((Math.Pow((x2-x1),2))+(Math.Pow((y2-y1),2)));
         Console.WriteLine($"{resp:F4}");
     }
-
 }
